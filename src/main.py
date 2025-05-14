fe6974b0bfdfc49a282c0eb7c67e63ecebb469d9
ladulcevitaconfeitaria <ladulcevitaconfeitaria@gmail.com>
2025-05-14 15:22:17 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import request, redirect
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run()
+@app.route("/cadastro", methods=["GET", "POST"])
+def cadastro():
+    if request.method == "POST":
+        nome = request.form["nome"]
+        email = request.form["email"]
+        senha = request.form["senha"]
+        print(f"Novo cadastro: {nome}, {email}")  # Só para testar
+        return redirect("/login")
+    return render_template("cadastro.html")
